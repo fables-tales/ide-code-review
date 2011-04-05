cbf4d7f22914c27f22db7fe2dc0f6600830d9cca
Sam Phippen <samphippen@googlemail.com>
2011-04-05 01:49:47 +0100

    add the first batch of code review.
    this includes documentation of coding standards, and a review of
    modules/admin.php
    
    include/feeds.php was touched because we found some stuff in there
    during code review
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- include/feeds.php
+++ include/feeds.php
@@ -1,1 +1,9 @@
 [not reviewed]
+
+[Code Sanity 5th April 2011]
+* Entire IDE will break if a user called blog-feeds is created related to ticket
+  #743
+* Improved readability of code in public method putFeeds(feedsArray)
+* Improved functionality of code in public method putFeeds(feedsArray) it now
+  throws an exception if it fails and will not return any values
+  
--- modules/admin.php
+++ modules/admin.php
@@ -1,1 +1,35 @@
-[not reviewed]
+[Reviewed on 5th April 2011]
+[Reviewed by Sam Phippen <samphippen@googlemail.com>]
+[Reviewed by Alistair Lynn <arplynn@gmail.com>]
+
+[Docstrings]
+* Added docstring to entire class
+* Added docstring to private username member
+* Added docstring to constructor
+* Improved docstring on public method saveTeamName(). Documents what the
+  function does in some detail, including how it interacts with TeamNameStrategy
+* Improved docstring on public methodgetBlogFeeds() based on looking into pete's
+  code, this needs review by him
+* Added docstrings to broken out methods from setFeedStatus()
+* Improved docstrings to setFeedStatus
+
+[Formatting]
+* Aligned whitespace in command installations in constructor
+* 80 column width in file
+
+[Code Sanity]
+* Make private method ensureAuthed() not return the AuthBackend. The phrase
+  ensureAuthed does not imply that you're going to get any related information
+  back
+* Replace an instance of "setOuptut" with "setOutput" in public method
+  setFeedStatus()
+
+
+[Public Method: setFeedStatus]
+* Restructure control flow to make failure case at end of file and not have
+  an early return by use of a conditional
+* Broke setting of status flags into it's own private static method
+  "updateFeedStatusFlagsByStatusString"
+* Realised all array processing logic was unnecessary due to php passing
+  objects by reference. Feed List update now performed by updating the feed 
+  object and rewriting the feed list to the feed file. Broken into rewriteMasterFeedsList()
